99380fd7307c673d631d62298cf0fc53f34aa61d
Vinicius <vinicius@diskingressos.com.br>
2026-07-21 15:07:10 -0300

    add22

--- curitiba360-backoffice/src/config/firebase.js
+++ curitiba360-backoffice/src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDemoKeyForCuritiba360TestingOnly",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "curitiba360-app.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "curitiba360-app",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "curitiba360-app.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1234567890",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1234567890:web:abcdef123456"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCejZgT4TT25H2oP4PH-8pD60gKYuZ4e3M",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "curitiba360defaut.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "curitiba360defaut",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "curitiba360defaut.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "34480527716",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:34480527716:web:3933cd6468762453874271"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
